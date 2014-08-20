bb05b0d861517bbb846b003414d6fe985b606b88
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-08-20 11:05:41 +0900

    Reflect review comments.

--- events/user_action_base.rb
+++ events/user_action_base.rb
@@ -1,93 +1,115 @@
-#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# Copyright 2014 TIS Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 require 'fileutils'
 require 'json'
-require 'find'
-require 'open3'
 
 class UserActionBase
+  CONDUCTOR_ROOT_DIR = '/opt/cloudconductor'
+  CONDUCTOR_PATTERNS_ROOT_DIR = File.join(CONDUCTOR_ROOT_DIR, 'patterns')
+  CONDUCTOR_PARAMETERS_FILENAME = 'parameters.json'
+  CONDUCTOR_PATTERN_TEMP_DIR = 'temp'
+  CONDUCTOR_PATTERN_RESOURCES_DIR = 'resources'
+  CONDUCTOR_PATTERN_CHEFREPO_DIR = 'chef-repo'
+  CONDUCTOR_PATTERN_ROLES_DIR = 'roles'
+  CONDUCTOR_PATTERN_COOKBOOKS_DIR = 'cookbooks'
+  CONDUCTOR_PATTERN_SITE_COOKBOOKS_DIR = 'site-cookbooks'
+  CONDUCTOR_SERF_PAYLOAD_FILENAME = 'payload.json'
 
-    def initialize(script_role, script_event)
-        @path_dictionary = {
-            cloudconductor_root_path: '/opt/cloudconductor',
-            parameters_file_name: 'parameters.json',
-            pattern_root_path: '/opt/cloudconductor/patterns',
-            temp_directory_name: 'temp',
-            log_directory_name: 'logs',
-            log_file_name: 'chef-solo.log',
-            log_level: 'info',
-            file_cache_directory_name: 'cache',
-            config_file_name: 'solo.rb',
-            node_file_name: 'node.json'
-        }
-        @roles = ENV['SERF_TAG_ROLE'].split(',')
-        @script_role = script_role
-        @script_event = script_event
-    end
+  CHEFSOLO_LOG_DIR = 'logs'
+  CHEFSOLO_LOG_FILENAME = 'chef-solo.log'
+  CHEFSOLO_LOG_LEVEL = 'info'
+  CHEFSOLO_FILECACHE_DIR = 'cache'
+  CHEFSOLO_CONFIG_FILENAME = 'solo.rb'
+  CHEFSOLO_NODE_FILENAME = 'node.json'
 
-    def create_config_file()
-        File.open(@path_dictionary[:config_file_path], 'w') do |file|
-            file.write "role_path '%{role_file_path}'\n" % @path_dictionary
-            file.write "log_level :%{log_level}\n" % @path_dictionary
-            file.write "log_location '%{log_path}/#{@script_role}_%{log_file_name}'\n" % @path_dictionary
-            file.write "file_cache_path '%{file_cache_path}'\n" % @path_dictionary
-            file.write "cookbook_path ['%{cookbooks_path}', '%{site_cookbooks_path}']\n" % @path_dictionary
-        end
-    end
+  def initialize(script_role, script_event)
+    @roles = ENV['SERF_TAG_ROLE'].split(',')
+    @script_role = script_role
+    @script_event = script_event
+    module_dir = File.realpath(File.dirname(__FILE__)).split(File::SEPARATOR)
+    pattern_name_index = module_dir.index('patterns') + 1
+    @pattern_name = module_dir[pattern_name_index]
+  end
 
-    def fetch_node_attributes
-        parameters = {}
-        begin
-            parameters = open("%{cloudconductor_root_path}/%{parameters_file_name}" % @path_dictionary) do |io|
-                JSON.load(io)
-            end
-        rescue
-        end
-        parameters
-    end
+  def update_and_read_parameters_file(pattern_dir)
+    payload_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_SERF_PAYLOAD_FILENAME)
+    parameters_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_PARAMETERS_FILENAME)
+    payload = {}
+    parameters = {}
+    payload = JSON.parse(File.read(payload_file)) if File.exist?(payload_file)
+    parameters = JSON.parse(File.read(parameters_file)) if File.exist?(parameters_file)
+    parameters.merge!(payload)
+    File.write(parameters_file, parameters.to_json)
+    parameters
+  end
 
-    def create_run_list
-        ["role[#{@script_role}_#{@script_event}]"]
-    end
+  def create_chefsolo_config_file(pattern_dir, chefsolo_log_dir, chefsolo_filecache_dir)
+    chefsolo_config_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
+    chefrepo_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
+    roles_dir = File.join(chefrepo_dir, CONDUCTOR_PATTERN_ROLES_DIR)
+    chefsolo_log_file = File.join(chefsolo_log_dir, "#{@script_role}_#{CHEFSOLO_LOG_FILENAME}")
+    cookbooks_dir = File.join(chefrepo_dir, CONDUCTOR_PATTERN_COOKBOOKS_DIR)
+    site_cookbooks_dir = File.join(chefrepo_dir, CONDUCTOR_PATTERN_SITE_COOKBOOKS_DIR)
 
-    def create_node_file
-        node_data = fetch_node_attributes
-        node_data[:run_list] = create_run_list
-        File.open(@path_dictionary[:node_file_path], 'w') do |file|
-          file.write node_data.to_json
-        end
+    File.open(chefsolo_config_file, 'w') do |file|
+      file.write("role_path '#{roles_dir}'\n")
+      file.write("log_level :#{CHEFSOLO_LOG_LEVEL}\n")
+      file.write("log_location '#{chefsolo_log_file}'\n")
+      file.write("file_cache_path '#{chefsolo_filecache_dir}'\n")
+      file.write("cookbook_path ['#{cookbooks_dir}', '#{site_cookbooks_dir}']\n")
     end
+    [chefrepo_dir, chefsolo_config_file]
+  end
 
-    def prepare_path pattern_path
-        @path_dictionary[:pattern_path] = pattern_path
-        @path_dictionary[:temp_path] = "%{pattern_path}/%{temp_directory_name}" % @path_dictionary
-        @path_dictionary[:config_file_path] = "%{temp_path}/%{config_file_name}" % @path_dictionary
-        @path_dictionary[:node_file_path] = "%{temp_path}/%{node_file_name}" % @path_dictionary
-        @path_dictionary[:log_path] = "%{temp_path}/%{log_directory_name}" % @path_dictionary
-        @path_dictionary[:file_cache_path] = "%{temp_path}/%{file_cache_directory_name}" % @path_dictionary
-        @path_dictionary[:chef_repo_path] = "%{pattern_path}/resources/chef-repo" % @path_dictionary
-        @path_dictionary[:role_file_path] = "%{chef_repo_path}/roles" % @path_dictionary
-        @path_dictionary[:cookbooks_path] = "%{chef_repo_path}/cookbooks" % @path_dictionary
-        @path_dictionary[:site_cookbooks_path] = "%{chef_repo_path}/site-cookbooks" % @path_dictionary
-        FileUtils.mkdir_p(@path_dictionary[:log_path])
-        FileUtils.mkdir_p(@path_dictionary[:file_cache_path])
-    end
+  def fetch_chefsolo_node_attributes
+    {}
+  end
 
-    def target?
-        @roles.include? @script_role
-    end
+  def create_chefsolo_run_list
+    ["role[#{@script_role}_#{@script_event}]"]
+  end
 
-    def exec
-        return if !target?
-        Dir.entries(@path_dictionary[:pattern_root_path]).select do |entry|
-            path = File.join(@path_dictionary[:pattern_root_path], entry)
-            if (File.directory? path) and !((entry =='.') || (entry == '..')) then
-                prepare_path path
-                create_config_file
-                create_node_file
-                out, err, status = Open3.capture3("cd %{chef_repo_path}; berks vendor ./cookbooks" % @path_dictionary)
-                out, err, status = Open3.capture3("chef-solo -c %{config_file_path} -j %{node_file_path}" % @path_dictionary)
-            end
-        end
-    end
+  def create_chefsolo_node_file(pattern_dir)
+    chefsolo_node_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
+    attributes = update_and_read_parameters_file(pattern_dir)
+    attributes.merge!(fetch_chefsolo_node_attributes)
+    attributes[:run_list] = create_chefsolo_run_list
+    File.write(chefsolo_node_file, attributes.to_json)
+    chefsolo_node_file
+  end
+
+  def create_chefsolo_directories(pattern_dir)
+    chefsolo_log_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_LOG_DIR)
+    chefsolo_filecache_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_FILECACHE_DIR)
+    FileUtils.mkdir_p(chefsolo_log_dir)
+    FileUtils.mkdir_p(chefsolo_filecache_dir)
+    [chefsolo_log_dir, chefsolo_filecache_dir]
+  end
+
+  def target?
+    @roles.include? @script_role
+  end
+
+  def execute
+    return unless target?
+    pattern_dir = File.join(CONDUCTOR_PATTERNS_ROOT_DIR, @pattern_name)
+    chefsolo_log_dir, chefsolo_filecache_dir = create_chefsolo_directories(pattern_dir)
+    chefrepo_dir, chefsolo_config_file = create_chefsolo_config_file(pattern_dir, chefsolo_log_dir, chefsolo_filecache_dir)
+    chefsolo_node_file = create_chefsolo_node_file(pattern_dir)
+    system("cd #{chefrepo_dir}; berks vendor #{CONDUCTOR_PATTERN_COOKBOOKS_DIR}")
+    system("chef-solo -c #{chefsolo_config_file} -j #{chefsolo_node_file}")
+  end
 end

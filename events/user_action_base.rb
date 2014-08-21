05547ade194d3a2d34a89fb83ae454d4b3239f52
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-08-21 21:00:43 +0900

    Add get_hosts and refactor the code.

--- events/user_action_base.rb
+++ events/user_action_base.rb
@@ -33,43 +33,76 @@ class UserActionBase
   CHEFSOLO_CONFIG_FILENAME = 'solo.rb'
   CHEFSOLO_NODE_FILENAME = 'node.json'
 
-  def initialize(script_role, script_event)
-    @roles = ENV['SERF_TAG_ROLE'].split(',')
-    @script_role = script_role
-    @script_event = script_event
+  def initialize(role, event, additional_parameters = {})
+    @role = role
+    @event = event
     script_file = File.expand_path(__FILE__)
-    @pattern_name = script_file.slice(%r{#{CONDUCTOR_PATTERNS_ROOT_DIR}/(?<pattern_name>[^/]*)}, 'pattern_name')
+    pattern_name = script_file.slice(%r{#{CONDUCTOR_PATTERNS_ROOT_DIR}/(?<pattern_name>[^/]*)}, 'pattern_name')
+    @pattern_dir = File.join(CONDUCTOR_PATTERNS_ROOT_DIR, pattern_name)
+    parameters = read_payload_file
+    parameters.merge!(additional_parameters)
+    update_parameters_file(parameters)
   end
 
-  def execute
-    return unless target?
-    pattern_dir = File.join(CONDUCTOR_PATTERNS_ROOT_DIR, @pattern_name)
-    create_chefsolo_directories(pattern_dir)
-    create_chefsolo_config_file(pattern_dir)
-    create_chefsolo_node_file(pattern_dir)
-    run_chefsolo(pattern_dir)
+  def execute(forced = false)
+    node_roles = ENV['SERF_TAG_ROLE'].split(',')
+    return unless forced || node_roles.include?(@role)
+    create_chefsolo_directories
+    create_chefsolo_config_file
+    create_chefsolo_node_file
+    run_chefsolo
+  end
+
+  def self.select_hosts(role)
+    hosts = []
+    serf_member_list = `serf members -tag 'role=[^,]*(,|)#{role}(,.*|)'`.strip
+    unless serf_member_list.empty?
+      serf_member_list.each_line do |serf_member|
+        host_and_port = serf_member.split(' ')[1]
+        host = host_and_port.split(':').first
+        hosts << host
+      end
+    end
+    hosts
   end
 
   private
 
-  def target?
-    @roles.include?(@script_role)
+  def read_payload_file
+    payload = {}
+    payload_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_SERF_PAYLOAD_FILENAME)
+    payload = JSON.parse(File.read(payload_file), symbolize_names: true) if File.exist?(payload_file)
+    payload
   end
 
-  def create_chefsolo_directories(pattern_dir)
-    chefsolo_log_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_LOG_DIR)
-    chefsolo_filecache_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_FILECACHE_DIR)
-    FileUtils.mkdir_p(chefsolo_log_dir)
-    FileUtils.mkdir_p(chefsolo_filecache_dir)
+  def read_parameters_file
+    parameters = {}
+    parameters_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_PARAMETERS_FILENAME)
+    parameters = JSON.parse(File.read(parameters_file), symbolize_names: true) if File.exist?(parameters_file)
+    parameters
   end
 
-  def create_chefsolo_config_file(pattern_dir)
-    chefsolo_log_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_LOG_DIR)
-    chefsolo_filecache_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_FILECACHE_DIR)
-    chefsolo_config_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
-    chefrepo_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
+  def update_parameters_file(new_parameters)
+    parameters_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_PARAMETERS_FILENAME)
+    parameters = read_parameters_file
+    parameters.merge!(new_parameters)
+    File.write(parameters_file, parameters.to_json)
+  end
+
+  def create_chefsolo_directories
+    chefsolo_log_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_LOG_DIR)
+    chefsolo_filecache_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_FILECACHE_DIR)
+    FileUtils.mkdir_p(chefsolo_log_dir) unless Dir.exist?(chefsolo_log_dir)
+    FileUtils.mkdir_p(chefsolo_filecache_dir) unless Dir.exist?(chefsolo_filecache_dir)
+  end
+
+  def create_chefsolo_config_file
+    chefsolo_log_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_LOG_DIR)
+    chefsolo_filecache_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_FILECACHE_DIR)
+    chefsolo_config_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
+    chefrepo_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
     roles_dir = File.join(chefrepo_dir, 'roles')
-    chefsolo_log_file = File.join(chefsolo_log_dir, "#{@script_role}_#{CHEFSOLO_LOG_FILENAME}")
+    chefsolo_log_file = File.join(chefsolo_log_dir, "#{@role}_#{CHEFSOLO_LOG_FILENAME}")
     cookbooks_dir = File.join(chefrepo_dir, 'cookbooks')
     site_cookbooks_dir = File.join(chefrepo_dir, 'site-cookbooks')
 
@@ -82,43 +115,18 @@ class UserActionBase
     end
   end
 
-  def create_chefsolo_node_file(pattern_dir)
-    chefsolo_node_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
-    attributes = update_and_read_parameters_file(pattern_dir)
-    attributes[:run_list] = create_chefsolo_run_list
+  def create_chefsolo_node_file
+    chefsolo_node_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
+    attributes = read_parameters_file
+    attributes[:run_list] = ["role[#{@role}_#{@event}]"]
     File.write(chefsolo_node_file, attributes.to_json)
     chefsolo_node_file
   end
 
-  def update_and_read_parameters_file(pattern_dir)
-    payload_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_SERF_PAYLOAD_FILENAME)
-    parameters_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_PARAMETERS_FILENAME)
-    payload = {}
-    parameters = {}
-    payload = JSON.parse(File.read(payload_file), symbolize_names: true) if File.exist?(payload_file)
-    parameters = JSON.parse(File.read(parameters_file), symbolize_names: true) if File.exist?(parameters_file)
-    parameters.merge!(payload)
-    parameters.merge!(
-      serf: {
-        agent: {
-          tags: {
-            role: ENV['SERF_TAG_ROLE']
-          }
-        }
-      }
-    )
-    File.write(parameters_file, parameters.to_json)
-    parameters
-  end
-
-  def create_chefsolo_run_list
-    ["role[#{@script_role}_#{@script_event}]"]
-  end
-
-  def run_chefsolo(pattern_dir)
-    chefrepo_dir = File.join(pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
-    chefsolo_config_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
-    chefsolo_node_file = File.join(pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
+  def run_chefsolo
+    chefrepo_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
+    chefsolo_config_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
+    chefsolo_node_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
     system("cd #{chefrepo_dir}; berks vendor ./cookbooks")
     system("chef-solo -c #{chefsolo_config_file} -j #{chefsolo_node_file}")
   end

61c1dd0bb9b80cb994e95260eb9c78493079b7ad
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-08-29 16:09:43 +0900

    Modify to use Consul for passing parameters.

--- events/user_action_base.rb
+++ events/user_action_base.rb
@@ -15,16 +15,22 @@
 
 require 'fileutils'
 require 'json'
+require 'logger'
+require 'rest-client'
+require 'base64'
 
 class UserActionBase
   CONDUCTOR_ROOT_DIR = '/opt/cloudconductor'
   CONDUCTOR_PATTERNS_ROOT_DIR = File.join(CONDUCTOR_ROOT_DIR, 'patterns')
-  CONDUCTOR_PARAMETERS_FILENAME = 'parameters.json'
   CONDUCTOR_PATTERN_TEMP_DIR = 'tmp'
   CONDUCTOR_PATTERN_RESOURCES_DIR = 'resources'
   CONDUCTOR_PATTERN_CHEFREPO_DIR = 'chef-repo'
   CONDUCTOR_PATTERN_ROLES_DIR = 'roles'
   CONDUCTOR_SERF_PAYLOAD_FILENAME = 'payload.json'
+  CONDUCTOR_CONSUL_KVS_URL = 'http://127.0.0.1:8500/v1/kv'
+  CONDUCTOR_CONSUL_KVS_NEW_PARAMETERS_URL = "#{CONDUCTOR_CONSUL_KVS_URL}/new_parameters"
+  CONDUCTOR_CONSUL_KVS_STORED_PARAMETERS_URL = "#{CONDUCTOR_CONSUL_KVS_URL}/stored_parameters"
+  CONDUCTOR_LOG_FILE = '/tmp/user_action_base.log'
 
   CHEFSOLO_LOG_DIR = 'logs'
   CHEFSOLO_LOG_FILENAME = 'chef-solo.log'
@@ -34,12 +40,17 @@ class UserActionBase
   CHEFSOLO_NODE_FILENAME = 'node.json'
 
   def initialize(role, event, additional_parameters = {})
+    @logger = Logger.new(CONDUCTOR_LOG_FILE)
     @role = role
     @event = event
+    @logger.debug("role = #{@role}")
+    @logger.debug("event = #{@event}")
+    @logger.debug("additional_parameters = #{additional_parameters}")
     script_file = File.expand_path(__FILE__)
     pattern_name = script_file.slice(%r{#{CONDUCTOR_PATTERNS_ROOT_DIR}/(?<pattern_name>[^/]*)}, 'pattern_name')
     @pattern_dir = File.join(CONDUCTOR_PATTERNS_ROOT_DIR, pattern_name)
-    parameters = read_payload_file
+    @logger.debug("pattern_dir = #{@pattern_dir}")
+    parameters = read_parameters(CONDUCTOR_CONSUL_KVS_NEW_PARAMETERS_URL)
     user_parameters = parameters[:parameters].nil? ? {} : parameters[:parameters]
     application_url = parameters[:url].nil? ? '' : parameters[:url]
     application_revision = parameters[:revision].nil? ? '' : parameters[:revision]
@@ -51,16 +62,27 @@ class UserActionBase
     }
     user_parameters.merge!(application_parameters)
     user_parameters.merge!(additional_parameters)
-    update_parameters_file(user_parameters)
+    update_parameters(CONDUCTOR_CONSUL_KVS_STORED_PARAMETERS_URL, user_parameters)
   end
 
   def execute(forced = false)
+    execution_result = false
+    @logger.debug("forced = #{forced}")
     node_roles = ENV['SERF_TAG_ROLE'].split(',')
+    @logger.debug("node_roles = #{node_roles}")
     return unless forced || node_roles.include?(@role)
-    create_chefsolo_directories
-    create_chefsolo_config_file
-    create_chefsolo_node_file
-    run_chefsolo
+    begin
+      create_chefsolo_directories
+      create_chefsolo_config_file
+      create_chefsolo_node_file
+      run_chefsolo
+      @logger.info('finished successfully.')
+      execution_result = true
+    rescue => exception
+      @logger.error("finished abnormally. #{exception.message}")
+      execution_result = false
+    end
+    execution_result
   end
 
   def self.select_hosts(role)
@@ -78,27 +100,34 @@ class UserActionBase
 
   private
 
-  def read_payload_file
-    payload = {}
-    payload_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_SERF_PAYLOAD_FILENAME)
-    payload = JSON.parse(File.read(payload_file), symbolize_names: true) if File.exist?(payload_file)
-    payload
-  end
-
-  def read_parameters_file
+  def read_parameters(url)
     parameters = {}
-    parameters_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CONDUCTOR_PARAMETERS_FILENAME)
-    parameters = JSON.parse(File.read(parameters_file), symbolize_names: true) if File.exist?(parameters_file)
+    begin
+      response = RestClient.get(url)
+      @logger.debug("response = #{response}")
+      response_hash = JSON.parse(response, symbolize_names: true).first
+      @logger.debug("response_hash = #{response_hash}")
+      parameters_json = Base64.decode64(response_hash[:Value])
+      @logger.debug("parameters_json = #{parameters_json}")
+      parameters = JSON.parse(parameters_json, symbolize_names: true)
+    rescue => exception
+      @logger.warn("failed to get the parameters[#{url}] from Consul KVS. #{exception.message}")
+    end
+    @logger.debug("read parameters = #{parameters}")
     parameters
   end
 
-  def update_parameters_file(new_parameters)
-    parameters_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR)
-    parameters_file = File.join(parameters_dir, CONDUCTOR_PARAMETERS_FILENAME)
-    parameters = read_parameters_file
-    parameters.merge!(new_parameters)
-    FileUtils.mkdir_p(parameters_dir) unless Dir.exist?(parameters_dir)
-    File.write(parameters_file, parameters.to_json)
+  def update_parameters(url, new_parameters)
+    @logger.debug("new_parameters = #{new_parameters}")
+    stored_parameters = read_parameters(url)
+    @logger.debug("stored_parameters = #{stored_parameters}")
+    stored_parameters.merge!(new_parameters)
+    @logger.debug("mergd_parameters = #{stored_parameters}")
+    begin
+      RestClient.put(url, stored_parameters.to_json)
+    rescue => exception
+      @logger.error("failed to put the parameters[#{url}] to Consul KVS. #{exception.message}")
+    end
   end
 
   def create_chefsolo_directories
@@ -117,7 +146,6 @@ class UserActionBase
     chefsolo_log_file = File.join(chefsolo_log_dir, "#{@role}_#{CHEFSOLO_LOG_FILENAME}")
     cookbooks_dir = File.join(chefrepo_dir, 'cookbooks')
     site_cookbooks_dir = File.join(chefrepo_dir, 'site-cookbooks')
-
     File.open(chefsolo_config_file, 'w') do |file|
       file.write("role_path '#{roles_dir}'\n")
       file.write("log_level :#{CHEFSOLO_LOG_LEVEL}\n")
@@ -129,7 +157,7 @@ class UserActionBase
 
   def create_chefsolo_node_file
     chefsolo_node_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
-    attributes = read_parameters_file
+    attributes = read_parameters(CONDUCTOR_CONSUL_KVS_STORED_PARAMETERS_URL)
     attributes[:run_list] = ["role[#{@role}_#{@event}]"]
     File.write(chefsolo_node_file, attributes.to_json)
     chefsolo_node_file
@@ -139,7 +167,10 @@ class UserActionBase
     chefrepo_dir = File.join(@pattern_dir, CONDUCTOR_PATTERN_RESOURCES_DIR, CONDUCTOR_PATTERN_CHEFREPO_DIR)
     chefsolo_config_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_CONFIG_FILENAME)
     chefsolo_node_file = File.join(@pattern_dir, CONDUCTOR_PATTERN_TEMP_DIR, CHEFSOLO_NODE_FILENAME)
-    system("cd #{chefrepo_dir}; berks vendor ./cookbooks")
-    system("chef-solo -c #{chefsolo_config_file} -j #{chefsolo_node_file}")
+    berks_result = system("cd #{chefrepo_dir}; berks vendor ./cookbooks")
+    @logger.debug("berks result = #{berks_result}")
+    chef_solo_result = system("chef-solo -c #{chefsolo_config_file} -j #{chefsolo_node_file}")
+    @logger.debug("chef-solo result = #{chef_solo_result}")
+    raise RuntimeError, 'chef-solo finished abnormally.' unless chef_solo_result;
   end
 end

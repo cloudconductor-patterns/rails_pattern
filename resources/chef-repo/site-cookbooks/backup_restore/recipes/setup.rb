7edc776ee255c97c522992bb464b86a9eeba8be1
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2014-09-04 17:20:46 +0900

    Install gem backup with options --no-ri --no-rdoc before include_recipe 'backup'

--- resources/chef-repo/site-cookbooks/backup_restore/recipes/setup.rb
+++ resources/chef-repo/site-cookbooks/backup_restore/recipes/setup.rb
@@ -1,4 +1,10 @@
 include_recipe 'cron'
+# install gem backup with options --no-ri --no-rdoc before include_recipe 'backup'
+gem_package 'backup' do
+  version node['backup']['version'] if node['backup']['version']
+  action :upgrade if node['backup']['upgrade?']
+  options '--no-ri --no-rdoc'
+end
 include_recipe 'backup'
 include_recipe 'percona::backup'
 

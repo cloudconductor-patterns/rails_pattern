c530f88118775d18da83c7c69186a7a4537c53a4
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2014-09-05 14:23:13 +0900

    Change default value of require_ssl to false

--- resources/chef-repo/site-cookbooks/mysql_part/attributes/default.rb
+++ resources/chef-repo/site-cookbooks/mysql_part/attributes/default.rb
@@ -6,4 +6,4 @@ default['mysql_part']['app']['username'] = 'rails'
 default['mysql_part']['app']['password'] = 'todo_replace_randompassword'
 default['mysql_part']['app']['encoding'] = 'utf8'
 default['mysql_part']['app']['privileges'] = [:all]
-default['mysql_part']['app']['require_ssl'] = true
+default['mysql_part']['app']['require_ssl'] = false

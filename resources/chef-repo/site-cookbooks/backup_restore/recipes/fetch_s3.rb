a16655448af0c0f43de05120f38f77ac15e45032
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2014-09-08 16:11:01 +0900

    Fix URI join step

--- resources/chef-repo/site-cookbooks/backup_restore/recipes/fetch_s3.rb
+++ resources/chef-repo/site-cookbooks/backup_restore/recipes/fetch_s3.rb
@@ -1,11 +1,12 @@
-# Find highest priority backup
+require 'uri'
+
 s3 = node['backup_restore']['destinations']['s3']
 tmp_dir = "#{node['backup_restore']['tmp_dir']}/restore"
 
 node['backup_restore']['restore']['target_sources'].each do |source_type|
   # Find latest backup on S3
   backup_name = "#{source_type}_full"
-  s3_path = "s3://#{s3['bucket']}#{s3['prefix']}/#{backup_name}/"
+  s3_path = URI.join("s3://#{s3['bucket']}", File.join(s3['prefix'], backup_name, '/')).to_s
   datetime_regexp = '[0-9]\{4\}.[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}/$'
   cmd = "/usr/bin/s3cmd ls #{s3_path} | grep '#{datetime_regexp}' | sort | awk 'END{print $2}'"
   latest_backup_path = `#{cmd}`.chomp

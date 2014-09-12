f0904f3074d9111bc29b0fedb99ccd36f59ce445
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-09-12 17:53:30 +0900

    Add ap-restore role to restore RAILS_ROOT

---
+++ resources/chef-repo/site-cookbooks/backup_restore/recipes/restore_ruby.rb
@@ -0,0 +1,13 @@
+tmp_dir = "#{node['backup_restore']['tmp_dir']}/restore"
+full_backup_name = 'ruby_full'
+backup_file = "#{tmp_dir}/#{full_backup_name}.tar"
+
+# TODO: Create LWRP
+
+bash 'extract_full_backup' do
+  code <<-EOF
+    tar -xvf #{backup_file} -C #{tmp_dir}
+    tar -zxvf #{tmp_dir}/#{full_backup_name}/archives/ruby.tar.gz -C #{node['rails_part']['app']['base_path']}
+  EOF
+  only_if { ::File.exist?(backup_file) && !::Dir.exist?("#{tmp_dir}/#{full_backup_name}") }
+end

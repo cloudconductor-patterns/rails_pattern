69ee6289605cc6771acfb00cf7493e651c7a735a
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-09-26 15:24:52 +0900

    Refactor pattern_executor.

--- pattern_executor.rb
+++ pattern_executor.rb
@@ -15,18 +15,30 @@
 
 require 'json'
 require 'active_support'
-require '/opt/cloudconductor/lib/pattern_util'
-require '/opt/cloudconductor/lib/consul_util'
+require '/opt/cloudconductor/lib/pattern/pattern_util'
+require '/opt/cloudconductor/lib/consul/consul_util'
 
 class PatternExecutor
   PATTERN_NAME = 'rails_pattern'
-  PATTERN_DIR = File.join(PatternUtil::PATTERNS_ROOT_DIR, PATTERN_NAME)
+  PATTERN_DIR = File.join(Pattern::PatternUtil::PATTERNS_ROOT_DIR, PATTERN_NAME)
   ROLES_DIR = File.join(PATTERN_DIR, 'roles')
+  SPEC_ROOT_DIR = File.join(PATTERN_DIR, 'serverspec')
+  SPEC_DIR = File.join(SPEC_ROOT_DIR, 'spec')
 
   def initialize(event)
-    @logger = PatternUtil.get_pattern_logger(PATTERN_NAME, 'executor.log')
+    @logger = Pattern::PatternUtil.get_pattern_logger(PATTERN_NAME, 'executor.log')
     @event = event
   end
+
+  def execute(node_role)
+    if @event == 'spec'
+      execute_serverspec(node_role)
+    else
+      execute_chef(node_role)
+    end
+  end
+
+  private
 
   def execute_chef(node_role)
     roles = node_role.split(',')
@@ -50,25 +62,42 @@ class PatternExecutor
     end
   end
 
-  private
+  def execute_serverspec(node_role)
+    roles = node_role.split(',')
+    roles.unshift('all')
+    events = ['configure']
+    events << 'deploy' if @action == 'deploy'
+    
+    roles.each do |role|
+      events.each do |event|
+        spec_file = "#{SPEC_DIR}/#{role}/#{role}_#{event}.json"
+        if File.exists?(spec_file)
+          @logger.info("execute serverspec with [#{spec_file}].")
+          spec_result = system("cd #{SPEC_ROOT_DIR}; rake spec[#{role},#{event}]")
+        else
+          @logger.info("spec file [#{spec_file}] does not exist. skipped.")
+        end
+      end
+    end
+  end
 
   def create_chefsolo_config_file(role)
     chefsolo_config_file = File.join(PATTERN_DIR, 'solo.rb')
-    chefsolo_log_file = File.join(PatternUtil::LOG_DIR, "#{PATTERN_NAME}_#{role}_chef-solo.log")
+    chefsolo_log_file = File.join(Pattern::PatternUtil::LOG_DIR, "#{PATTERN_NAME}_#{role}_chef-solo.log")
     cookbooks_dir = File.join(PATTERN_DIR, 'cookbooks')
     site_cookbooks_dir = File.join(PATTERN_DIR, 'site-cookbooks')
     File.open(chefsolo_config_file, 'w') do |file|
       file.write("role_path '#{ROLES_DIR}'\n")
       file.write("log_level :info\n")
       file.write("log_location '#{chefsolo_log_file}'\n")
-      file.write("file_cache_path '#{PatternUtil::FILECACHE_DIR}'\n")
+      file.write("file_cache_path '#{Pattern::PatternUtil::FILECACHE_DIR}'\n")
       file.write("cookbook_path ['#{cookbooks_dir}', '#{site_cookbooks_dir}']\n")
     end
   end
 
   def create_chefsolo_node_file(role)
     chefsolo_node_file = File.join(PATTERN_DIR, 'node.js')
-    parameters = ConsulUtil.read_parameters
+    parameters = Consul::ConsulUtil.read_parameters
     attributes = parameters[:cloudconductor][:patterns][PATTERN_NAME.to_sym][:user_attributes]
     attributes[:run_list] = ["role[#{role}_#{@event}]"]
     File.write(chefsolo_node_file, attributes.to_json)
@@ -96,4 +125,4 @@ end
 
 role = ARGV[0]
 event = ARGV[1]
-PatternExecutor.new(event).execute_chef(role)
+PatternExecutor.new(event).execute(role)

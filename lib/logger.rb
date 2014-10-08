65357a744aa5eda558b20e9bb4db95b8b3680f1f
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-10-08 09:38:12 +0100

    Modify pattern-execution interface.

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,4 +1,4 @@
-#!/bin/sh
+# -*- coding: utf-8 -*-
 # Copyright 2014 TIS Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,5 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+require 'logger'
 
-ruby pattern_executor.rb "${1}" "${2}"
+module RailsPattern
+  class Logger
+    def self.logger(log_filename)
+      logger = ::Logger.new(log_filename)
+      logger.formatter = proc do |severity, datetime, _progname, message|
+        "[#{datetime.strftime('%Y-%m-%dT%H:%M:%S')}] #{severity}: #{message}\n"
+      end
+      logger
+    end
+  end
+end

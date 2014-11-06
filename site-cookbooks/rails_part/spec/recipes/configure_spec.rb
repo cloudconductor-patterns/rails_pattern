aae9c11249d6976ba91e8f7f46206d1324602eed
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-11-06 03:07:08 +0000

    Fix to match of development Terms

--- site-cookbooks/rails_part/spec/recipes/configure_spec.rb
+++ site-cookbooks/rails_part/spec/recipes/configure_spec.rb
@@ -1,23 +1,31 @@
 require_relative '../spec_helper'
 
 describe 'rails_part::configure' do
-  let(:chef_run) do
-    runner = ChefSpec::SoloRunner.new(
-      cookbook_path: %w(site-cookbooks cookbooks),
-      platform:      'centos',
-      version:       '6.5'
-    )do |node|
-      node.set['postfix']['sasl'] = 'foo'
-      node.set['postfix']['sasl_password_file'] = '/etc/postfix/sasl_passwd'
+  let(:chef_run) { ChefSpec::SoloRunner.new }
+
+  describe 'enable sasl auth for smtp' do
+    before do
+      stub_command('/usr/bin/test /etc/alternatives/mta -ef /usr/sbin/sendmail.postfix').and_return(0)
+  
+      chef_run.node.set['postfix']['main']['smtp_sasl_auth_enable'] = 'yes'
+      chef_run.node.set['postfix']['sasl']['smtp_sasl_passwd'] = 'password'
+      chef_run.node.set['postfix']['sasl']['smtp_sasl_user_name'] = 'username' 
+      chef_run.converge(described_recipe)
     end
-    runner.converge('rails_part::configure')
+  
+    it 'include sasl_auth' do
+      expect(chef_run).to include_recipe 'postfix::sasl_auth'
+    end
   end
 
-  before do
-    stub_command('/usr/bin/test /etc/alternatives/mta -ef /usr/sbin/sendmail.postfix').and_return(0)
-  end
+  describe 'disable sasl auth for smtp' do
+    before do
+      chef_run.node.set['postfix']['main']['smtp_sasl_auth_enable'] = 'no'
+      chef_run.converge(described_recipe)
+    end
 
-  it 'include sasl_auth' do
-    expect(chef_run).to include_recipe 'postfix::sasl_auth'
+    it 'sasl_auth recipe of postfix cookbook is not include' do
+      expect(chef_run).to_not include_recipe 'postfix::sasl_auth'
+    end
   end
 end
--- site-cookbooks/rails_part/spec/recipes/setup_spec.rb
+++ site-cookbooks/rails_part/spec/recipes/setup_spec.rb
@@ -1,44 +1,45 @@
 require_relative '../spec_helper'
 
 describe 'rails_part::setup' do
-  let(:chef_run) do
-    ChefSpec::SoloRunner.new(
-      cookbook_path: %w(site-cookbooks cookbooks),
-      platform:      'centos',
-      version:       '6.5'
-    ).converge('rails_part::setup')
-  end
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  it 'install git' do
+  it 'include git recipe' do
     expect(chef_run).to include_recipe 'git'
   end
 
-  it 'install build-essential' do
+  it 'include build-essential for rhel' do
+    expect(chef_run.node[:platform_family]).to eq('rhel')
     expect(chef_run).to include_recipe('build-essential::_rhel')
   end
 
-  it 'install mysql 5.6' do
+  it 'include mysql56 recipe of um-mysql-community cookbook' do
     expect(chef_run).to include_recipe('yum-mysql-community::mysql56')
   end
 
-  it 'install mysql-community-devel' do
+  it 'install mysql-community-devel package' do
     expect(chef_run).to install_package('mysql-community-devel')
   end
 
-  it 'install sqlite-devel' do
+  it 'install sqlite-devel package' do
     expect(chef_run).to install_package('sqlite-devel')
   end
 
-  it 'rbenv setup' do
+  it 'include rbenv_setup recipe' do
     expect(chef_run).to include_recipe 'rails_part::rbenv_setup'
   end
-  it 'ruby-shadow' do
 
+  it 'install ruby-shadow gem' do
     expect(chef_run).to install_gem_package 'ruby-shadow'
   end
 
   it 'create user' do
-    expect(chef_run).to create_user('rails').with(
+    chef_run.node.set[:rails_part][:user][:name] = 'chefspec_user'
+    chef_run.node.set[:rails_part][:user][:passwd] = 'chefspec_pwd'
+    chef_run.node.set[:rails_part][:user][:manage_home] = true
+    chef_run.converge(described_recipe)
+    
+    expect(chef_run).to create_user('chefspec_user').with(
+      password: 'chefspec_pwd',
       supports: {
         manage_home: true
       }
@@ -46,8 +47,12 @@ describe 'rails_part::setup' do
   end
 
   it 'create group' do
-    expect(chef_run).to create_group('rails').with(
-      members: ['rails'],
+    chef_run.node.set[:rails_part][:user][:group] = 'chefspec_group'
+    chef_run.node.set[:rails_part][:user][:name] = 'chefspec_user'
+    chef_run.converge(described_recipe)
+
+    expect(chef_run).to create_group('chefspec_group').with(
+      members: ['chefspec_user'],
       append: true
     )
   end

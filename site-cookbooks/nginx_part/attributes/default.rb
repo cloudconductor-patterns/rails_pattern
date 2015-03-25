cbc88b750cc1e10110a4f30684cfd90aeb6ee3fd
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-03-25 17:08:34 +0900

    Fix foodcritic violation

--- site-cookbooks/nginx_part/attributes/default.rb
+++ site-cookbooks/nginx_part/attributes/default.rb
@@ -6,7 +6,7 @@ default['nginx']['repo_source'] = 'nginx'
 default['nginx']['default_site_enabled'] = false
 
 include_attribute 'nginx_conf'
-default[:nginx_conf][:locations] = {}
+default['nginx_conf']['locations'] = {}
 
 # 'cloudconductor' attributes
 # include_attribute 'cloudconductor'
